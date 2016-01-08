af122167967c4793dc050f7ad0f5573fe198bef2
ccoenraets <ccoenraets@gmail.com>
2016-01-08 18:26:56 -0500

    change callback url

--- modules/auth.js
+++ modules/auth.js
@@ -3,7 +3,7 @@ var nforce = require('nforce');
 var org = nforce.createConnection({
   clientId: '3MVG9sG9Z3Q1Rlbc4tkIx2fI3ZUDVyYt86Ypl8ZqBXTpzPbQNHxq7gpwKcN75BB.fpgHxzSWgwgRY6nVfvBUe',
   clientSecret: '735250516407528507',
-  redirectUri: 'https://slackforce.herokuapp.com/oauth/callback'
+  redirectUri: 'https://slackforce.herokuapp.com/auth/callback'
 });
 
 function login(req, res) {
