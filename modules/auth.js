b433d5bde76ba85e9ff38f3cf54dd56c5dedd8d8
ccoenraets <ccoenraets@gmail.com>
2016-01-08 18:48:53 -0500

    change callback url

--- modules/auth.js
+++ modules/auth.js
@@ -15,8 +15,13 @@ function callback(req, res) {
 }
 
 function hello(req, res) {
-    res.send("Hello");
-}
+    var q = 'SELECT Id, Name FROM Contact LIMIT 1';
+    org.query({ query: q }, function(err, resp){
+    if(!err && resp.records) {
+        var contact = resp.records[0];
+        res.send({text: contact.Name});
+    }
+});
 
 exports.login = login;
 exports.callback = callback;
