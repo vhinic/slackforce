8f7bc60ce92593a4f82e613b2ce9ea8ec3392c54
ccoenraets <ccoenraets@gmail.com>
2016-01-10 11:28:51 -0500

    format message

--- modules/auth.js
+++ modules/auth.js
@@ -1,17 +1,17 @@
-var nforce = require('nforce');
+var nforce = require('nforce'),
 
-var CLIENT_ID = process.env.CLIENT_ID;
-var CLIENT_SECRET = process.env.CLIENT_SECRET;
-var USER_ID = process.env.USER_ID;
-var PASSWORD = process.env.PASSWORD;
-var CONTACT_TOKEN = process.env.CONTACT_TOKEN;
+    CLIENT_ID = process.env.CLIENT_ID,
+    CLIENT_SECRET = process.env.CLIENT_SECRET,
+    USER_ID = process.env.USER_ID,
+    PASSWORD = process.env.PASSWORD,
+    CONTACT_TOKEN = process.env.CONTACT_TOKEN,
 
-var org = nforce.createConnection({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-  redirectUri: 'https://slackforce.herokuapp.com/auth/callback',
-  mode: 'single'
-});
+    org = nforce.createConnection({
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      redirectUri: 'https://slackforce.herokuapp.com/auth/callback',
+      mode: 'single'
+    });
 
 org.authenticate({ username: USER_ID, password: PASSWORD}, function(err, resp) {
     if (err) {
@@ -23,21 +23,31 @@ org.authenticate({ username: USER_ID, password: PASSWORD}, function(err, resp) {
 });
 
 function contact(req, res) {
-
     if (req.body.token != CONTACT_TOKEN) {
         res.send("Invalid token");
         return;
     }
-
-    var q = "SELECT Id, Name, Account.Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
-    org.query({ query: q }, function(err, resp) {
-        if(!err && resp.records) {
-            var contact = resp.records;
-            res.send(contact);
-            //res.send({"text": contact.name});
+    var q = "SELECT Id, Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
+    org.query({query: q}, function(err, resp) {
+        if (err) {
+            console.error(err);
+            res.send("An error as occurred");
+            return;
+        }
+        if (resp.records && resp.records.length>0) {
+            var contacts = resp.records;
+            var attachments = [];
+            contacts.forEach(function(contact) {
+                var fields = [];
+                fields.push({title: "Name", value: contact.Name, short:true});
+                fields.push({title: "Phone", value: contact.Phone, short:true});
+                attachments.push({fields: fields});
+            });
+            res.send(JSON.stringify({attachments: attachments}));
+        } else {
+            res.send("No records");
         }
     });
-
 }
 
 exports.contact = contact;
