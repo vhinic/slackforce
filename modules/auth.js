1e81b7e7bb43adf587c5de51ba19c602ca3bbfa4
ccoenraets <ccoenraets@gmail.com>
2016-01-09 21:55:43 -0500

    uid/pwd oauth flow

--- modules/auth.js
+++ modules/auth.js
@@ -1,54 +1,44 @@
 var nforce = require('nforce');
 
+var CLIENT_ID = process.env.CLIENT_ID;
+var CLIENT_SECRET = process.env.CLIENT_SECRET;
+var USER_ID = process.env.USER_ID;
+var PASSWORD = process.env.PASSWORD;
+var CONTACT_TOKEN = process.env.CONTACT_TOKEN;
+
 var org = nforce.createConnection({
-  clientId: '3MVG9sG9Z3Q1Rlbc4tkIx2fI3ZUDVyYt86Ypl8ZqBXTpzPbQNHxq7gpwKcN75BB.fpgHxzSWgwgRY6nVfvBUe',
-  clientSecret: '735250516407528507',
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
   redirectUri: 'https://slackforce.herokuapp.com/auth/callback'
 });
 
-function login(req, res) {
-    res.redirect(org.getAuthUri());
-}
-
-function callback(req, res) {
-    var session = req.session;
-    org.authenticate({code: req.query.code}, function(err, resp){
-        if(!err) {
-          console.log('Access Token: ' + resp.access_token);
-          session.oauth = resp;
-          res.send("Authentication succeeded");
-        } else {
-          console.log('Error: ' + err.message);
-          res.send("Authentication failed");
-        }
-    });
-}
-
-function hello(req, res) {
-
-    if (req.body) {
-        console.log(req.body);
-        res.end(JSON.stringify(req.body, null, 2));
+org.authenticate({ username: USER_ID, password: PASSWORD}, function(err, resp) {
+    if (err) {
+        console.error("Authentication error");
+        console.error(err);
     } else {
-        res.send("request doesn't have body");
+        console.log("Authentication successful");
     }
+});
 
-    /*
-    if (!req.session || !req.session.oauth) {
-        res.send("Not authenticated");
+function contact(req, res) {
+
+    if (req.body.token != CONTACT_TOKEN) {
+        res.send("Invalid token");
         return;
     }
+
     var q = 'SELECT Id, Name FROM Contact LIMIT 1';
-    org.query({ query: q, oauth: req.session.oauth }, function(err, resp) {
+    org.query({ query: q }, function(err, resp) {
         if(!err && resp.records) {
             var contact = resp.records[0];
             res.send(contact);
             //res.send({"text": contact.name});
         }
     });
-    */
+
 }
 
 exports.login = login;
 exports.callback = callback;
-exports.hello = hello;
+exports.contact = contact;
--- server.js
+++ server.js
@@ -1,37 +1,13 @@
 var express = require('express'),
-    cors = require('cors'),
     bodyParser = require('body-parser'),
-    compression = require('compression'),
     auth = require('./modules/auth'),
-    session      = require('express-session'),
-    cookieParser = require('cookie-parser'),
     app = express();
-
-var options = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "credentials": true
-};
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.urlencoded());
-//app.use(cookieParser());
-//app.use(session({ secret: 'somesecret', key: 'sid' }));
-//app.use(compression());
-//app.use(cors(options));
 
-app.get('/auth/login', auth.login);
-app.get('/auth/callback', auth.callback);
-app.post('/hello', auth.hello);
-
-/*
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send(err);
-});
-*/
+app.post('/contact', auth.contact);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
