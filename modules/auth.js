712b139b8b1cedd8a82a91a5ccf9f7b6ce18544b
ccoenraets <ccoenraets@gmail.com>
2016-01-10 22:37:31 -0500

    adding case command

--- modules/auth.js
+++ modules/auth.js
@@ -3,8 +3,7 @@ var nforce = require('nforce'),
     CLIENT_ID = process.env.CLIENT_ID,
     CLIENT_SECRET = process.env.CLIENT_SECRET,
     USER_ID = process.env.USER_ID,
-    PASSWORD = process.env.PASSWORD,
-    CONTACT_TOKEN = process.env.CONTACT_TOKEN,
+    PASSWORD = process.env.PASSWORD;
 
     org = nforce.createConnection({
       clientId: CLIENT_ID,
@@ -13,45 +12,16 @@ var nforce = require('nforce'),
       mode: 'single'
     });
 
-org.authenticate({ username: USER_ID, password: PASSWORD}, function(err, resp) {
-    if (err) {
-        console.error("Authentication error");
-        console.error(err);
-    } else {
-        console.log("Authentication successful");
-    }
-});
-
-function contact(req, res) {
-
-    if (req.body.token != CONTACT_TOKEN) {
-        res.send("Invalid token");
-        return;
-    }
-
-    var q = "SELECT Id, Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
-    org.query({query: q}, function(err, resp) {
+function login() {
+    org.authenticate({ username: USER_ID, password: PASSWORD}, function(err, resp) {
         if (err) {
+            console.error("Authentication error");
             console.error(err);
-            res.send("An error as occurred");
-            return;
-        }
-        if (resp.records && resp.records.length>0) {
-            var contacts = resp.records;
-            var attachments = [];
-            contacts.forEach(function(contact) {
-                var fields = [];
-                fields.push({title: "Name", value: contact.get("Name"), short:true});
-                fields.push({title: "Phone", value: contact.get("Phone"), short:true});
-                fields.push({title: "Mobile", value: contact.get("MobilePhone"), short:true});
-                fields.push({title: "Email", value: contact.get("Email"), short:true});
-                attachments.push({color: "#009cdb", fields: fields});
-            });
-            res.json({text: "Contacts matching '" + req.body.text + "':", attachments: attachments});
         } else {
-            res.send("No records");
+            console.log("Authentication successful");
         }
     });
 }
 
-exports.contact = contact;
+exports.login = login;
+exports.org = org;
---
+++ modules/case.js
@@ -0,0 +1,30 @@
+var nforce = require('nforce'),
+    org = require('./auth').org,
+
+    CASE_TOKEN = process.env.CASE_TOKEN;
+
+function execute(req, res) {
+
+    if (req.body.token != CASE_TOKEN) {
+        res.send("Invalid token");
+        return;
+    }
+
+    var c = nforce.createSObject('Case');
+    c.set('subject', req.body.subject);
+    c.set('description', req.body.description);
+    c.set('origin', 'Slack');
+    c.set('status', 'New');
+
+    org.insert({ sobject: c}, function(err, resp){
+        if (err) {
+            console.error(err);
+            res.send("An error occurred while creating a case");
+        } else {
+            console.log(resp);
+            res.send('Case created');
+        }
+    });
+}
+
+exports.case = case;
