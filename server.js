f4b8d360126183e5a36e824e41ab8f921695560b
ccoenraets <ccoenraets@gmail.com>
2016-01-10 22:43:56 -0500

    adding case command

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     auth = require('./modules/auth'),
     contact = require('./modules/contact'),
-    case = require('./modules/case'),
+    case_ = require('./modules/case'),
     app = express();
 
 app.set('port', process.env.PORT || 5000);
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.urlencoded());
 
 app.post('/contact', contact.execute);
-app.post('/case', case.execute);
+app.post('/case', case_.execute);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
